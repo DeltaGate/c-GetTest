fc7199c7b6688d957ff9bc9a114504886b041fe5
Connor Adamson <connor@conkat.co.uk>
2024-08-28 18:10:36 +0100

    Added Deserialization
    Can now deserialize Json data so it can be read by specific class objects

---
+++ wizardTrackerBasic/wizardTrackerBasic/SpellResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace wizardTrackerBasic
+{
+    public class SpellResponse
+    {
+        [JsonPropertyName("desc")]
+        public string[] descript {  get; set; }  //class object with sepcific json property linked name
+    }
+}
