3c094eed9c3520346c11d4b72bf4280affa617f6
Connor Adamson <connor@conkat.co.uk>
2024-08-29 17:50:51 +0100

    Completed SQL backend work, prep complete for WPF application
    Command Line app now acts as a full test bed for backend features for my WPF app, 
    
    Access's the 5E SRD API
    
    GET's Json response and extracts data from it with Deserialize command from System.Text.Json libary
    
    takes data and places it into a locally housed data containing collumns for each nessesary varible housed in JSon data (uses 2 seperate SQL querys depending if certain data is nessesary)
    
    all prep now complete, using this test bed I will now create a new project file through Visual Studio for a WPF front end application and implument features above to program.

--- wizardTrackerBasic/Program.cs
+++ wizardTrackerBasic/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
 using System.Text.Json;
+using wizardTrackerBasic;
+using Microsoft.Data.Sqlite;  // using Nuget Package
 
 namespace WizardsRule
 { 
@@ -8,6 +10,14 @@ namespace WizardsRule
 
         static void Main(string[] args)
         {
+
+            // connect to database
+            var dBLocation = "Data Source=C:\\Users\\Connor\\Desktop\\coding 2024\\c# interview work\\wizardTrackerBasic\\Spells - Copy.db"; //db location
+            var connection = new SqliteConnection(dBLocation); // create object with location of db
+            connection.Open(); //open connection
+
+
+
             Console.WriteLine("Please Enter the spell your wish to recieve:");
             string scrySpell = Console.ReadLine();
 
@@ -17,11 +27,77 @@ namespace WizardsRule
 
                 var endPoint = new Uri("https://www.dnd5eapi.co/api/spells/" + scrySpell); //sets 
 
-                var result = client.GetAsync(endPoint).Result.Content.ReadAsStringAsync().Result;
+                var result = client.GetAsync(endPoint).Result.Content;
                 //telling httpClient obj to send a get call to spell page, and send back content as a readable string, the .result tells it to wait for the result as by default async is all at once so won't wait otherwise.
                  
-                Console.WriteLine(result);
+                Console.WriteLine(result.ReadAsStringAsync().Result);
 
+                string held = result.ReadAsStringAsync().Result;
+
+                //JsonObject damageDetails = new JsonObject(result.ReadAsStringAsync().Result);
+
+                SpellResponse arcaneKnowledge = JsonSerializer.Deserialize<SpellResponse>(held); //deserialise or extract json data by catagory to object
+                Console.WriteLine(arcaneKnowledge.desc[0]); // display specific data.
+
+                arcaneKnowledge.descAsString = string.Join(" ", arcaneKnowledge.desc);
+                arcaneKnowledge.descAsString = arcaneKnowledge.descAsString.Replace("'", "");
+
+
+                Console.WriteLine("--------");
+                Console.WriteLine(arcaneKnowledge.descAsString);
+                Console.WriteLine("--------");
+
+
+                arcaneKnowledge.higher_levelAsString = string.Join(" ", arcaneKnowledge.higher_level);
+                arcaneKnowledge.higher_levelAsString = arcaneKnowledge.higher_levelAsString.Replace("'", "");
+
+                arcaneKnowledge.componentsAsString = string.Join(" ", arcaneKnowledge.components);
+                arcaneKnowledge.componentsAsString = arcaneKnowledge.componentsAsString.Replace("'", "");
+
+                if(arcaneKnowledge.material != null) { arcaneKnowledge.material = arcaneKnowledge.material.Replace("'", ""); }
+
+
+
+
+                if(arcaneKnowledge.damage == null) { var addToDB = new SqliteCommand("INSERT INTO unprepared (name, desc, higher_level, range, components, material, ritual, duration, concentration, casting_time, level) VALUES('" + arcaneKnowledge.name + "', '" + arcaneKnowledge.descAsString + "', '" + arcaneKnowledge.higher_levelAsString + "', '" + arcaneKnowledge.range + "', '" + arcaneKnowledge.componentsAsString + "', '" + arcaneKnowledge.material + "', '" + arcaneKnowledge.ritual + "', '" + arcaneKnowledge.duration + "', '" + arcaneKnowledge.concentration + "', '" + arcaneKnowledge.casting_time + "', '" + arcaneKnowledge.level + "');", connection);
+                    addToDB.ExecuteNonQuery();
+                }
+                else
+                {
+                    var addToDB = new SqliteCommand("INSERT INTO unprepared (name, desc, higher_level, range, components, material, ritual, duration, concentration, casting_time, level, attack_type, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10) VALUES('" + arcaneKnowledge.name + "', '" + arcaneKnowledge.descAsString + "', '" + arcaneKnowledge.higher_levelAsString + "', '" + arcaneKnowledge.range + "', '" + arcaneKnowledge.componentsAsString + "', '" + arcaneKnowledge.material + "', '" + arcaneKnowledge.ritual + "', '" + arcaneKnowledge.duration + "', '" + arcaneKnowledge.concentration + "', '" + arcaneKnowledge.casting_time + "', '" + arcaneKnowledge.level + "', '" + arcaneKnowledge.damage.damage_type.name + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l1 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l2 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l3 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l4 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l5 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l6 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l7 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l8 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l9 + "', '" + arcaneKnowledge.damage.damage_at_slot_level.l10 + "');", connection);
+
+                    addToDB.ExecuteNonQuery();
+                }
+                
+
+
+
+
+                // runs sql query to insert data into db
+
+
+
+
+                var checkTables = new SqliteCommand("SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY 1", connection);
+
+                var reader = checkTables.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+                //runs sql query to read through table names and display them to console
+
+                var checkSpellList = new SqliteCommand("SELECT * FROM unprepared", connection);
+
+                reader = checkSpellList.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+
+                // same as above, but cycles through the spell list
             }
 
         }
---
+++ wizardTrackerBasic/SpellResponse.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace wizardTrackerBasic
+{
+    public class damage_at_slot_level
+    {
+        [JsonPropertyName("1")]
+        public string l1 { get; set; }
+
+        [JsonPropertyName("2")]
+        public string l2 { get; set; }
+
+        [JsonPropertyName("3")]
+        public string l3 { get; set; }
+
+        [JsonPropertyName("4")]
+        public string l4 { get; set; }
+
+        [JsonPropertyName("5")]
+        public string l5 { get; set; }
+
+        [JsonPropertyName("6")]
+        public string l6 { get; set; }
+
+        [JsonPropertyName("7")]
+        public string l7 { get; set; }
+
+        [JsonPropertyName("8")]
+        public string l8 { get; set; }
+
+        [JsonPropertyName("9")]
+        public string l9 { get; set; }
+
+        [JsonPropertyName("10")]
+        public string l10 { get; set; }
+
+    }
+
+    public class damage_type  // subsubclass, same as below but down a level :)
+    {
+        public string name { get; set; }
+
+    }
+    public class damage         // subclass accessed and used when required by main class
+    {
+        public damage_type damage_type {  get; set; }
+
+        public damage_at_slot_level damage_at_slot_level { get; set; }
+    }
+    public class SpellResponse
+    {
+
+        public string name {  get; set; }
+
+        [JsonPropertyName("desc")]
+        public string[] desc {  get; set; }  //class object with sepcific json property linked name
+        public string[] higher_level { get; set; }
+        public string range { get; set; }
+        public string[] components { get; set; }
+        public string material { get; set; }
+        public bool ritual { get; set; }
+        public string duration { get; set; }
+        public bool concentration { get; set; }
+        public string casting_time { get; set; }
+        public int level { get; set; }
+        public damage damage { get; set; } // create damage varible of class damage
+
+
+        // converted propeties
+        public string descAsString { get; set; }
+        public string higher_levelAsString {  get; set; }
+        public string componentsAsString {  get; set; }
+        public string damage_at_slot_levelAsString { get; set; }
+
+
+    }
+
+}
